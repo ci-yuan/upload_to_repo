1eda006fb183f4c1bb8deb3cd613ddfdbc5d826f
ciyuan <msciyuan@outlook.com>
2021-09-16 17:56:37 +0800

    fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "Upload to Repo"
+echo "#### Upload to Repo ####"
 # time=$(date)
 # echo "::set-output name=time::$time"
 
 # 参数
-if [ -z "$INPUT_ARGS" ]; then
-  echo 'Required Args'
-  exit 1
-fi
+# if [ -z "$INPUT_ARGS" ]; then
+#   echo 'Required Args'
+#   exit 1
+# fi
 
 # 执行
 ./upload $INPUT_LOCAL_PATH $INPUT_USERNAME $INPUT_ACCESS_TOKEN $INPUT_REPO $INPUT_REPO_PATH $INPUT_COMMIT_MESSAGE
