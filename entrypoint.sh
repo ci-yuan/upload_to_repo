316a079ad2686f6f26fbc5db7bfe382080981ef0
ciyuan <msciyuan@outlook.com>
2021-09-16 18:54:41 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,8 @@ echo "#### Upload to Repo ####"
 #   exit 1
 # fi
 
+ls /
 ls
-
+ls worksapce
 # 执行
 ./worksapce/upload $INPUT_LOCAL_PATH $INPUT_USERNAME $INPUT_ACCESS_TOKEN $INPUT_REPO $INPUT_REPO_PATH $INPUT_COMMIT_MESSAGE
