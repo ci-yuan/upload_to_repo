83b58c0218ba32421d3183f951e904ba3268cee8
ciyuan <msciyuan@outlook.com>
2021-09-16 17:07:15 +0800

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,11 @@ echo "Upload to Repo"
 # time=$(date)
 # echo "::set-output name=time::$time"
 
+# 参数
+if [ -z "$INPUT_ARGS" ]; then
+  echo 'Required Args'
+  exit 1
+fi
+
+# 执行
+./upload $LOCAL_PATH $USERNAME $ACCESS_TOKEN $REPO $REPO_PATH $COMMIT_MESSAGE
